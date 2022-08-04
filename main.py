31d87cd3ac8889e369608c79c5a835b5ab343f91
Daniel Stotts <dstodev@gmail.com>
2022-08-04 07:37:19 -0700

    Updates main.py to use multiple processes

--- main.py
+++ main.py
@@ -1,26 +1,61 @@
 import argparse
+import enum
+import multiprocessing as mp
 
 from command import Prompt
 
 
+class Signal(enum.Enum):
+    QUIT = enum.auto()
+    UNKNOWN = enum.auto()
+
+
 def main():
-    prompt = Prompt()
+    ctx = mp.get_context()
+    input_strings = ctx.Queue()
+    responses = ctx.Queue()
+    process = ctx.Process(target=process_commands, args=(input_strings, responses))
+    process.start()
+
+    response = None
+
+    while response != Signal.QUIT:
+        input_string = input('Input a command: ')
+        input_strings.put(input_string)
+        response = responses.get()
+        try:
+            print(*response)
+        except TypeError:
+            print(response)
+
+    process.join()
+
+
+def process_commands(input_strings: mp.Queue, responses: mp.Queue):
     resume = True
 
     def quit_handler(*_args):
         nonlocal resume
         resume = False
+        responses.put(Signal.QUIT)
 
     def echo_handler(*args):
-        print(*args)
+        responses.put(args)
 
+    def default_handler(*_args):
+        # The main process will block waiting for a response,
+        # so even unrecognized commands must put some response
+        responses.put(Signal.UNKNOWN)
+
+    prompt = Prompt()
+    prompt.register_default(default_handler)
     prompt.register_command('quit', quit_handler)
     prompt.register_command('q', quit_handler)
     prompt.register_command('echo', echo_handler)
 
     while resume:
-        input_string = input('Input a command: ')
         try:
+            input_string = input_strings.get()
             prompt.process_input_string(input_string)
         except ValueError as e:
             print(*e.args)
